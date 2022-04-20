3542b5a0433bcd582eea344ce40e1f32846fc2d7
Charles Dalisay <charlesdalisay004@gmail.com>
2022-04-20 10:28:52 +0800

    [5] Create service request table

--- EMPRAPPDB.sql
+++ EMPRAPPDB.sql
@@ -7,7 +7,7 @@ GO
 CREATE TABLE Department(
 	 DepartmentID int IDENTITY(1,1) PRIMARY KEY,
 	 DepartmentName varchar(70) NOT NULL,
-	 DateCreated DATETIME NOT NULL,
+	 DateCreated datetime NOT NULL,
 	 CreatedBy varchar(20) NOT NULL
 );
 
@@ -28,7 +28,7 @@ GO
 CREATE TABLE AssignmentType (
 	 AssignmentTypeID int IDENTITY(1,1) PRIMARY KEY,
 	 AssignmentType varchar(70) NOT NULL,
-	 DateCreated DATETIME NOT NULL,
+	 DateCreated datetime NOT NULL,
 	 CreatedBy varchar(20) NOT NULL
 );
 
@@ -48,7 +48,7 @@ GO
 CREATE TABLE SRType (
 	 SRTypeID int IDENTITY(1,1) PRIMARY KEY,
 	 SRType varchar(70) NOT NULL,
-	 DateCreated DATETIME NOT NULL,
+	 DateCreated datetime NOT NULL,
 	 CreatedBy varchar(20) NOT NULL
 );
 
@@ -72,22 +72,40 @@ CREATE TABLE Developer (
 	 FirstName varchar(50) NOT NULL,
 	 MiddleName varchar(50),
 	 LastName varchar(50) NOT NULL,
-	 DateHire DATETIME NOT NULL,
-	 isActive BIT NOT NULL,
+	 DateHire datetime NOT NULL,
+	 isActive bit NOT NULL,
 	 MetrobankEmail varchar(50),
 	 Homeddress varchar(200),
 	 CityProvince varchar(50),
 	 ContactNumber varchar(20),
-	 DepartmentID INT NOT NULL FOREIGN KEY REFERENCES Department(DepartmentID),
-	 SupervisorID INT FOREIGN KEY REFERENCES Developer(DeveloperID),
-	 DateCreated DATETIME NOT NULL,
+	 DepartmentID int NOT NULL FOREIGN KEY REFERENCES Department(DepartmentID),
+	 SupervisorID int FOREIGN KEY REFERENCES Developer(DeveloperID),
+	 DateCreated datetime NOT NULL,
 	 CreatedBy varchar(20) NOT NULL,
-	 DateModified DATETIME,
+	 DateModified datetime,
 	 ModifiedBy varchar(20) 
 );
 
 GO
 SP_HELP Developer
+
+GO
+CREATE TABLE ServiceRequest(
+	ServiceRequestID int IDENTITY(1,1) PRIMARY KEY,
+	SRNo varchar(12) NOT NULL,
+	Title varchar(100) NOT NULL,
+	Description varchar(150) NOT NULL,
+	SRTypeID int NOT NULL FOREIGN KEY REFERENCES SRType(SRTypeID),
+	Status varchar(10) NOT NULL,
+	TargetYear int NOT NULL,
+	DateCreated datetime NOT NULL,
+	CreatedBy varchar(20) NOT NULL,
+	DateModified datetime,
+	ModifiedBy varchar(20) 
+);
+
+GO
+SP_HELP ServiceRequest
 
 
 
