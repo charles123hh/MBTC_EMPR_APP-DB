9faa9f66da60b666b5c505d262bb7b3f5f351b7a
Charles Dalisay <charlesdalisay004@gmail.com>
2022-04-20 09:43:54 +0800

    [3] Hot fix create additional refrence table

--- EMPRAPPDB.sql
+++ EMPRAPPDB.sql
@@ -24,3 +24,47 @@ VALUES('NET', GETDATE(), 'Default'),
 GO
 SELECT * FROM Department
 
+GO
+CREATE TABLE AssignmentType (
+	 AssignmentTypeID int IDENTITY(1,1) PRIMARY KEY,
+	 AssignmentType varchar(70) NOT NULL,
+	 DateCreated DATETIME NOT NULL,
+	 CreatedBy varchar(20) NOT NULL
+);
+
+GO
+SP_HELP AssignmentType
+
+GO
+INSERT INTO AssignmentType(AssignmentType, DateCreated, CreatedBy)
+VALUES('Supervisor', GETDATE(), 'Default'), 
+	  ('Team Lead', GETDATE(), 'Default'),
+	  ('Developer', GETDATE(), 'Default')
+
+GO
+SELECT * FROM AssignmentType
+
+GO
+CREATE TABLE SRType (
+	 SRTypeID int IDENTITY(1,1) PRIMARY KEY,
+	 SRType varchar(70) NOT NULL,
+	 DateCreated DATETIME NOT NULL,
+	 CreatedBy varchar(20) NOT NULL
+);
+
+GO
+SP_HELP SRType
+
+GO
+INSERT INTO SRType(SRType, DateCreated, CreatedBy)
+VALUES('DPR', GETDATE(), 'Default'), 
+	  ('PER', GETDATE(), 'Default'),
+	  ('MNT', GETDATE(), 'Default'),
+	  ('OPS', GETDATE(), 'Default')
+
+GO
+SELECT * FROM SRType
+
+
+
+
